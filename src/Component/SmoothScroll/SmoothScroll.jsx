f15552c061e98b32c1590f05435f352fb554fca3
vimal-t-dev <vimalthirugnanam@gmail.com>
2026-09-23 00:19:07 +0530

    Fast Scrolling 8

--- src/Component/SmoothScroll/SmoothScroll.jsx
+++ src/Component/SmoothScroll/SmoothScroll.jsx
@@ -12,10 +12,10 @@ function SmoothScroll({ children }) {
 
       // Touch inertia
       syncTouchLerp: 0.08,
-      touchInertiaExponent: 1.8,
+      touchInertiaExponent: 1.7,
 
       // Touch sensitivity
-      touchMultiplier: 1.2,
+      touchMultiplier: 1.1,
 
       // Mouse wheel sensitivity
       wheelMultiplier: 1,
